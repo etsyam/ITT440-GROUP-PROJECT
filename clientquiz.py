fa40c94274627cb1227727eeb8f5953344d0ca8a
Chaerool <73098044+Khairunaquib@users.noreply.github.com>
2021-01-11 10:22:14 +0800

    client quiz

---
+++ clientquiz.py
@@ -0,0 +1,30 @@
+import socket
+import select
+import sys
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+if len(sys.argv) != 3:
+    print "Print in the following order : script, IP address, port number"
+    exit()
+
+ip_addr = "192.168.1.39"
+port = 8080
+server.connect((ip_addr, port))
+
+while True:
+
+    # maintains a list of possible input streams
+    sockets_list = [sys.stdin, server]
+
+    read_sockets,write_socket, error_socket = select.select(sockets_list,[],[])
+
+    for socks in read_sockets:
+        if socks == server:
+            message = socks.recv(2048)
+            print message
+        else:
+            message = sys.stdin.readline()
+            server.send(message)
+            sys.stdout.flush()
+server.close()
+sys.exit()
