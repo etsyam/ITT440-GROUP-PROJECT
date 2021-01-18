313ffa072d524e57d1eda3f3d4b0147df1b16eed
hamizan98 <73188945+hamizan98@users.noreply.github.com>
2021-01-18 22:15:11 +0800

    Update serverquiz.py

--- serverquiz.py
+++ serverquiz.py
@@ -5,7 +5,6 @@ import sys
 import time
 import random
 
-
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 ip_add="192.168.1.39"
@@ -15,28 +14,27 @@ server.listen(100)
 
 list_of_clients=[]
 
+soalan = [" Bangunan apakah yang tertinggi di Malaysia? \n a.KLCC b.KL Tower c.Menara Maybank d.Menara CIMB",
+     " Bilakah tarikh kemerdekaan di Malaysia? \n a.31/8 b.31/9 c.25/7 d.26/5",
+     " Di manakah terletaknya Aquaria? \n a.Selangor b.Johor c.Terengganu d.KL",
+     " Tahun apakah kini di Malaysia? \n a.1997 b.1998 c.2020 d.2021",
+     " Berapakah hitungan Perdana Menteri di Malaysia sejak kemerdekaan? \n a.7 b.8 c.9 d.10"]
 
-Q = [" What is the tallest building in Malaysia? \n a.KLCC b.KL Tower c.Menara Maybank d.Menara CIMB",
-     " At what date is our National Day? \n a.31/8 b.31/9 c.25/7 d.26/5",
-     " Where is Aquaria? \n a.Selangor b.Johor c.Terengganu d.KL",
-     " What is the year now in Malaysia \n a.1997 b.1998 c.2020 d.2021",
-     " How many prime minister do we have? \n a.7 b.8 c.9 d.10"]
+jawapan = ['a', 'a', 'd', 'd', 'b']
 
-A = ['a', 'a', 'd', 'd', 'b']
-
-Count=[]
+calculate=[]
 client = ["address",-1]
-bzr =[0, 0, 0]
+buzzer =[0, 0, 0]
 
 def clientthread(conn, addr):
-    conn.send("\nSelamat Datang ke Kahuurr Kuiz! Sila jawab soalan sebelum player lain cuba menjawab!!\n Press any key on the keyboard and ENTER as a buzzer for the given question\n")
+    conn.send("\nSelamat Datang ke Kahuurr Kuiz! Sila jawab soalan sebelum pemain lain cuba menjawab!!\n Tekan apa sahaja key di papan kekunci anda dan tekan enter untuk buzzer\n")
     
     while True:
             message = conn.recv(2048)
             if message:
-                if bzr[0]==0:
+                if buzzer[0]==0:
                     client[0] = conn
-                    bzr[0] = 1
+                    buzzer[0] = 1
                     i = 0
                     while i < len(list_of_clients):
                             if list_of_clients[i] == client[0]:
@@ -44,32 +42,32 @@ def clientthread(conn, addr):
                             i +=1
                     client[1] = i
 
-                elif bzr[0] ==1 and conn==client[0]:
-                        bol = message[0] == A[bzr[2]][0]
-                        print (A[bzr[2]][0])
-                        if bol:
-                            broadcast("player" + str(client[1]+1) + " +1" + "\n\n")
-                            Count[i] += 1
-                            if Count[i]==5:
-                                broadcast("player" + str(client[1]+1) + " WON" + "\n")
-                                end_quiz()
+                elif buzzer[0] ==1 and conn==client[0]:
+                        kira = message[0] == jawapan[buzzer[2]][0]
+                        print (jawapan[buzzer[2]][0])
+                        if kira:
+                            broadcast("Pemain" + str(client[1]+1) + " +1" + "\n\n")
+                            calculate[i] += 1
+                            if calculate[i]==5:
+                                broadcast("pemain" + str(client[1]+1) + " WON" + "\n")
+                                end_soalanquiz()
                                 sys.exit()
 
                         else:
-                            broadcast("player" + str(client[1]+1) + " +0" + "\n\n")
-                            Count[i] += 0
-                        bzr[0]=0
-                        if len(Q) != 0:
-                            Q.pop(bzr[2])
-                            A.pop(bzr[2])
-                        if len(Q)==0:
-                            end_quiz()
-                        quiz()
+                            broadcast("Pemain" + str(client[1]+1) + " +0" + "\n\n")
+                            calculate[i] += 0
+                        buzzer[0]=0
+                        if len(soalan) != 0:
+                            soalan.pop(buzzer[2])
+                            jawapan.pop(buzzer[2])
+                        if len(soalan)==0:
+                            end_soalanquiz()
+                        soalanquiz()
 
                 else:
-                        conn.send(" player " + str(client[1]+1) + " pressed buzzer first\n\n")
+                        conn.send(" Pemain " + str(client[1]+1) + " tekan buzzer!!\n\n")
             else:
-                    remove(conn)
+                    tamat(conn)
 
 def broadcast(message):
     for clients in list_of_clients:
@@ -77,35 +75,36 @@ def broadcast(message):
             clients.send(message)
         except:
             clients.close()
-            remove(clients)
-def end_quiz():
-        broadcast("Game Over\n")
-        bzr[1]=1
-        i = Count.index(max(Count))
-        broadcast("player " + str(i+1)+ " wins!! by scoring "+str(Count[i])+" points.")
+            tamat(clients)
+def end_soalanquiz():
+        broadcast("Permainan Tamat\n")
+        buzzer[1]=1
+        i = calculate.index(max(calculate))
+        broadcast("pemain " + str(i+1)+ " menang dengan markah "+str(calculate[i])+" mata.")
         for x in range(len(list_of_clients)):
-            list_of_clients[x].send("You scored " + str(Count[x]) + " points.")
+            list_of_clients[x].send("Anda dapat " + str(calculate[x]) + " mata.")
             
         server.close()
 
 
-def quiz():
-    bzr[2] = random.randint(0,10000)%len(Q)
-    if len(Q) != 0:
+def soalanquiz():
+    buzzer[2] = random.randint(0,10000)%len(soalan)
+    if len(soalan) != 0:
         for connection in list_of_clients:
-            connection.send(Q[bzr[2]])
-def remove(connection):
+            connection.send(soalan[buzzer[2]])
+def tamat(connection):
     if connection in list_of_clients:
-        list_of_clients.remove(connection)
+        list_of_clients.tamat(connection)
 
 
 while True:
     conn, addr = server.accept()
     list_of_clients.append(conn)
-    Count.append(0)
+    calculate.append(0)
     print (addr[0] + " connected")
     start_new_thread(clientthread,(conn,addr))
     if(len(list_of_clients)==3):
-        quiz()
+        soalanquiz()
 conn.close()
 server.close()
+
